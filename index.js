68ec7f907fb11ae21d20da8224503a27766e21a5
Daniel Cousens <github@dcousens.com>
2015-03-25 14:04:38 +1100

    index: remove offsets

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var through = require('through')
 module.exports = function BlkDatStream() {
   var buffers = []
   var buffer = new Buffer(0)
+
+  var blockLength
   var remaining = 0
   var needed = 0
 
@@ -17,8 +19,6 @@ module.exports = function BlkDatStream() {
     buffer = Buffer.concat([buffer].concat(buffers), remaining)
     buffers = []
 
-    var offset = 0
-
     do {
       // do we need to parse a magic header?
       if (needed === 0) {
@@ -27,39 +27,32 @@ module.exports = function BlkDatStream() {
 
         // read the magic header (magic number, block length)
         var magicInt = buffer.readUInt32LE(0)
-        var blockLength = buffer.readUInt32LE(4)
+        blockLength = buffer.readUInt32LE(4)
 
         if (magicInt !== 0xd9b4bef9) {
           throw new Error('Unexpected data')
         }
 
-        // now, block length is what is needed
-        needed = blockLength
-        remaining -= 8
-        offset = 8
+        // block length is what is needed further
+        // but we don't truncate, so we also need the magic header
+        needed = 8 + blockLength
 
         // and loop
         continue
       }
 
       // read the block
-      var block = buffer.slice(offset, offset + needed)
+      var block = buffer.slice(8, blockLength)
 
       // process block
       this.queue(block)
 
-      // update cursor information
-      buffer = buffer.slice(offset + needed)
-      remaining -= needed
+      // update stream information
+      buffer = buffer.slice(needed)
+      remaining = buffer.length
       needed = 0
 
     // do we [still] have enough data?
     } while (remaining >= needed)
-
-    // truncate Buffer if magic header was read
-    if (needed !== 0) {
-      buffer = buffer.slice(offset)
-      remaining = buffer.length
-    }
   })
 }
